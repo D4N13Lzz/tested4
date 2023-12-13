bfb81420841ffd082c7d03fbbe6b75b0e7a11f44
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-13 09:35:28 -0300

    Update db.php

--- api/db.php
+++ api/db.php
@@ -17,6 +17,13 @@ if ($conn->connect_error) {
 $query = "SELECT id, nome, email, idade FROM usuarios";
 $result = $conn->query($query);
 
+// Configurações de CORS
+header("Access-Control-Allow-Origin: *"); // Permite solicitações de qualquer origem
+// Outros cabeçalhos CORS
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+// Configuração do método permitido (no caso, apenas GET)
+header("Access-Control-Allow-Methods: GET");
+
 // Verifica se a consulta foi bem-sucedida
 if ($result) {
     // Inicializa um array para armazenar os resultados
@@ -27,20 +34,16 @@ if ($result) {
         $usuarios[] = $row;
     }
 
+    // Configuração do cabeçalho Content-Type
+    header('Content-Type: application/json');
+
     // Retorna os dados como JSON
     echo json_encode($usuarios);
 } else {
     // Retorna um erro como JSON
+    header('Content-Type: application/json');
     echo json_encode(array('error' => 'Erro na consulta: ' . $conn->error));
 }
-
-header('Content-Type: application/json');
-// Configurações de CORS
-header("Access-Control-Allow-Origin: *"); // Permite solicitações de qualquer origem
-// Outros cabeçalhos CORS
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-// Configuração do método permitido (no caso, apenas GET)
-header("Access-Control-Allow-Methods: GET");
 
 // Fecha a conexão
 $conn->close();
