5d3e4f5b4ecbefc638d8405543c4f7ada87b3e73
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-13 08:10:56 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página PHP Simples</title>
+</head>
+<body>
+
+<?php
+// Obtém a data e hora atual
+$dataHoraAtual = date('d/m/Y H:i:s');
+
+// Mensagem de boas-vindas
+$mensagem = "Olá Mundo! A data e hora atual são: $dataHoraAtual";
+?>
+
+<!-- Exibe a mensagem na página -->
+<h1><?php echo $mensagem; ?></h1>
+
+</body>
+</html>
