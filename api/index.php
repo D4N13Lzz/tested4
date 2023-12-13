87c00b8a2f9b2202d8f3ce7e32d8bc74a59e7895
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-13 09:04:08 -0300

    teste

--- api/index.php
+++ api/index.php
@@ -13,24 +13,23 @@
 // Incluir o arquivo de conexão com o banco de dados
 include 'db.php';
 
-// Consulta SQL para obter dados da tabela "usuarios"
-$sql = "SELECT * FROM usuarios";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
+// Verificar se a consulta foi bem-sucedida
+if (isset($usuarios) && !empty($usuarios)) {
     // Exibir os dados em uma tabela
     echo "<table border='1'>
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
                 <th>Email</th>
+                <th>Idade</th>
             </tr>";
 
-    while ($row = $result->fetch_assoc()) {
+    foreach ($usuarios as $usuario) {
         echo "<tr>
-                <td>" . $row["id"] . "</td>
-                <td>" . $row["nome"] . "</td>
-                <td>" . $row["email"] . "</td>
+                <td>" . $usuario["id"] . "</td>
+                <td>" . $usuario["nome"] . "</td>
+                <td>" . $usuario["email"] . "</td>
+                <td>" . $usuario["idade"] . "</td>
               </tr>";
     }
 
@@ -38,9 +37,6 @@ if ($result->num_rows > 0) {
 } else {
     echo "Nenhum usuário encontrado na tabela.";
 }
-
-// Fechar a conexão com o banco de dados
-$conn->close();
 ?>
 
 </body>
