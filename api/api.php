5fd8f5a913cba845dc65240063ca048fe7992294
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-22 20:28:16 -0300

    Update api.php

--- api/api.php
+++ api/api.php
@@ -1,5 +1,8 @@
 <?php
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 $data = array('message' => 'Olá da API PHP!');
 echo json_encode($data);
