2f64084892222cfd4c1c00da9b0e5bd9f0695975
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-22 20:40:42 -0300

    Update usuario.php

--- api/usuario.php
+++ api/usuario.php
@@ -4,19 +4,27 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-require_once('db.php'); // Inclua seu arquivo db.php
+require_once('db.php');
 
-$sql = "SELECT email, name FROM usuarios";
-$result = $conn->query($sql);
+// Verifica se a tabela existe
+$table_name = 'usuarios';
+$check_table_query = "SHOW TABLES LIKE '$table_name'";
+$table_exists = $conn->query($check_table_query)->num_rows > 0;
 
-if ($result->num_rows > 0) {
-    $data = array();
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
-    echo json_encode($data);
+if (!$table_exists) {
+    echo json_encode(array('error' => 'Tabela de usuários não encontrada.'));
 } else {
-    echo json_encode(array());
+    // Insere dados fictícios
+    $insert_query = "INSERT INTO $table_name (email, name) VALUES
+        ('usuario1@example.com', 'Usuário 1'),
+        ('usuario2@example.com', 'Usuário 2'),
+        ('usuario3@example.com', 'Usuário 3')";
+    
+    if ($conn->query($insert_query) === TRUE) {
+        echo json_encode(array('success' => 'Dados inseridos com sucesso.'));
+    } else {
+        echo json_encode(array('error' => 'Erro ao inserir dados: ' . $conn->error));
+    }
 }
 
 $conn->close();
