9f7871c46d05bc51e8001b80818868d003bfa4c3
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-22 20:16:42 -0300

    api

--- api/api.php
+++ api/api.php
@@ -1,18 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
-
-include('db.php');
-
-$result = $conn->query("SELECT * FROM Usuarios");
-$users = array();
-
-while ($row = $result->fetch_assoc()) {
-    $users[] = $row;
-}
-
 header('Content-Type: application/json');
-echo json_encode($users);
+
+$data = array('message' => 'Olá da API PHP!');
+echo json_encode($data);
 ?>
