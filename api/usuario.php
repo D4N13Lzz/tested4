34efccc3945631d38bcce7f371f49a1f7bd385c1
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-22 20:37:07 -0300

    test db

---
+++ api/usuario.php
@@ -0,0 +1,23 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+require_once('db.php'); // Inclua seu arquivo db.php
+
+$sql = "SELECT email, name FROM usuarios";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $data = array();
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+    echo json_encode($data);
+} else {
+    echo json_encode(array());
+}
+
+$conn->close();
+?>
