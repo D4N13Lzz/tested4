47780042a0f0fb0951b67cb3068f7145325ff3d6
D4N13Lzz <125717711+D4N13Lzz@users.noreply.github.com>
2023-12-22 20:53:17 -0300

    testewwww

--- api/usuarios.php
+++ api/usuarios.php
@@ -1,8 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
+header('Content-Type: application/json');
+
 require_once('db.php');
 
 $table_name = 'usuarios';
@@ -12,29 +10,19 @@ $check_table_query = "SHOW TABLES LIKE '$table_name'";
 $table_exists = $conn->query($check_table_query)->num_rows > 0;
 
 if (!$table_exists) {
-    echo 'Tabela de usuários não encontrada.';
+    echo json_encode(array('error' => 'Tabela de usuários não encontrada.'));
 } else {
     $sql = "SELECT email, name FROM $table_name";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        echo '<h2>Dados da Tabela "usuarios"</h2>';
-        echo '<table border="1">
-                <tr>
-                    <th>Email</th>
-                    <th>Name</th>
-                </tr>';
-
+        $data = array();
         while ($row = $result->fetch_assoc()) {
-            echo '<tr>
-                    <td>' . $row['email'] . '</td>
-                    <td>' . $row['name'] . '</td>
-                  </tr>';
+            $data[] = $row;
         }
-
-        echo '</table>';
+        echo json_encode($data);
     } else {
-        echo 'Nenhum usuário encontrado na tabela.';
+        echo json_encode(array());
     }
 }
 
